410016f4996ff85d6ce1ad16cab1ea116d5c7a2f
navaneethv0909-jpg <navaneethv0909@gmail.com>
2026-02-10 21:07:29 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,32 @@
+const carData = {
+    "Maruti Suzuki": {
+        "Alto 800": "796 cc",
+        "WagonR": "998 cc",
+        "Swift": "1197 cc",
+        "Brezza": "1462 cc"
+    },
+    "Hyundai": {
+        "i10": "1086 cc",
+        "i20": "1197 cc",
+        "Verna": "1497 cc",
+        "Creta": "1497 cc"
+    },
+    "Tata": {
+        "Nano": "624 cc",
+        "Tiago": "1199 cc",
+        "Nexon": "1497 cc",
+        "Harrier": "1956 cc"
+    },
+    "Honda": {
+        "Amaze": "1199 cc",
+        "City": "1498 cc",
+        "WR-V": "1498 cc",
+        "Elevate": "1498 cc"
+    },
+    "Mahindra": {
+        "Bolero": "1493 cc",
+        "Scorpio": "2184 cc",
+        "XUV700": "1999 cc"
+    }
+};
 
